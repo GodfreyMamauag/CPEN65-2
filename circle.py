77ac7b8b3c369c9f93aef02343d8464054454efd
Mark Godfrey C. Mamauag <90832711+GodfreyMamauag@users.noreply.github.com>
2022-10-08 14:28:12 +0800

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,10 @@
+import math
+#Create a Python program that computes the area of a circle
+
+def area_of_circle(radius):
+    area_of_circle = 3.141592653589793238 * radius * radius
+    return area_of_circle
+
+
+radius = int(input("Enter the value of radius:"))
+print("The area of the circle is: ", area_of_circle(radius))
